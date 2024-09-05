81013d7141020046e260821ae145f6b36e8f03fe
knkp <knkp934@gmail.com>
2024-09-05 14:11:55 -0400

    adding required changes to compile linux4microchip br for SAM9X60 devices

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Example flake environment for build buildroot projects";
+  description = "Modified Buildroot flake to handle nuvoton builds";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -21,6 +21,49 @@
               unzip
               pkg-config
               wget
+	      patch 
+	      perl 
+	      wget 
+	      cpio 
+	      unzip 
+	      rsync 
+	      bc
+	      libxcrypt # provides <crypt.h> and -lcrypt (used on some host utils to generate a password)
+	      gettext   # provide 'msginit' used by host tools for build
+	      #these are directly from dep checks on buildroot for linux4microchip extensions for the sam9x60, probably these are under different names, but we'll see
+	      cmake # it tries to use this, but doesn't complain about not finding it, adding it anyway in case it helps with other deps
+libbpf
+libpwquality #pwquality
+libseccomp
+libselinux
+libapparmor
+pam
+rPackages.PAMmisc #pam_misc
+libmicrohttpd
+#crypt_activate_by_token_pin # can't find this one
+curl.dev #libcurl
+libidn2
+libidn
+libiptcdata #libiptc
+qrencode.dev #libqrencode
+#libgcrypt-config, should be provided by libgcrypt
+libgcrypt
+libgpg-error #gpg-error
+gnutls
+p11-kit #p11-kit-1
+libfido2
+tpm2-tools #tss2-esys (I think)
+#tss2-rc
+#tss2-mu
+#tss2-tcti-device
+#libdw # not sure about this one presently (maybe elfutils and libunwind: https://github.com/NixOS/nixpkgs/issues/31940) ?
+bzip2
+xz.dev #liblzma
+lz4.dev #liblz4
+libxkbcommon #xkbcommon
+python3
+valgrind
+	      git-repo
               pkgsCross.aarch64-multiplatform.gccStdenv.cc
             ] ++ pkgs.linux.nativeBuildInputs);
         }).env;
